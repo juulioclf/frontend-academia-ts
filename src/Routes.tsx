b43378a902cdac1d23b917c33bef4c9aab887470
Júlio Ferreira <juliolima731@gmail.com>
2024-03-01 13:27:00 -0400

    fix: route component name

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,11 +2,12 @@ import React, { ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
+import Initial from './Pages/Initital';
 
 function Router(): ReactElement {
   return (
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Initial/>} />
       </Routes>
   );
 }
