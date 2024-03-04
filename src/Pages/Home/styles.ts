3de793614012f143e89c7016fb1fe9f27383aecf
Júlio Ferreira <juliolima731@gmail.com>
2024-03-04 11:10:37 -0400

    pre-feat: home page

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,2 +1,23 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    display: flex;
+    position: absolute;
+    background-color: white;
+    border-radius: 10px;
+    width: 1150px;
+    height: 700px;
+    top: 151px;
+    left: 35em;
+`
+
+export const SecondContainer = styled.div`
+    display: flex;
+    position: absolute;
+    border-radius: 15px;
+    top: 160px;
+    left: 20px;
+    width: 1110px;
+    height: 520px;
+    border: 1px solid black;
+`
